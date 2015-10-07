1f1974907ede5ee3d6ad7b9eaf239d088e894ad8
Maxired (Maxence Dalmais) <github@maxired.fr>
2015-10-07 16:01:08 +0200

    fix test

--- test/beanstalk-transport.test.js
+++ test/beanstalk-transport.test.js
@@ -11,10 +11,10 @@ var test = require('seneca-transport-test')
 
 describe('beanstalk-transport', function () {
   it('happy-any', function (fin) {
-    test.foo_test('beanstalk-transport', require, fin, 'beanstalk', -11300)
+    test.foo_test('', require, fin, 'beanstalk', -11300)
   })
 
   it('happy-pin', function (fin) {
-    test.foo_pintest('beanstalk-transport', require, fin, 'beanstalk', -11300)
+    test.foo_pintest('', require, fin, 'beanstalk', -11300)
   })
 })
